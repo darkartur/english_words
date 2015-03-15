360693c76f904775ceccae067987d56e389a206d
Arthur <artur.udalov@gmail.com>
2015-03-15 14:24:06 +0300

    Spliting sentences by "?", "!" and "."

--- src/sentence/sentence.test.ts
+++ src/sentence/sentence.test.ts
@@ -5,13 +5,32 @@ import _ = require("underscore");
 import assert = require("assert");
 import Sentence = require("./sentence");
 
-
-
-
 describe('Sentence', () => {
     describe("#parse()", () => {
-        it('One sentence text should be converted to array of one sentence', () => {
-            assert.deepEqual(arrayToString(Sentence.parse("Be or not to be.")), ["Be or not to be."])
+
+        it('Parsing one sentence', () => {
+            assert.deepEqual(arrayToString(Sentence.parse("Be or not to be")), ["Be or not to be"])
+        });
+
+        it('Parsing two sentences separated by ".w', () => {
+            assert.deepEqual(
+                arrayToString(Sentence.parse("Sentence #1. Sentence #2")),
+                ["Sentence #1.", "Sentence #2"]
+            );
+        });
+
+        it('Parsing two sentences separated by "?"', () => {
+            assert.deepEqual(
+                arrayToString(Sentence.parse("Question? Answer")),
+                ["Question?", "Answer"]
+            );
+        });
+
+        it('Sentences separated with "!"', () => {
+            assert.deepEqual(
+                arrayToString(Sentence.parse("Alarm! Ok")),
+                ["Alarm!", "Ok"]
+            );
         });
 
         function arrayToString(sentences: Sentence[]) {
--- src/sentence/sentence.ts
+++ src/sentence/sentence.ts
@@ -1,3 +1,7 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+import _ = require("underscore");
+
 class Sentence {
 
     constructor(private text: string) {
@@ -9,9 +13,33 @@ class Sentence {
     }
 
     static parse(text: string): Sentence[] {
-        return [new Sentence(text)];
+        return _.map<string,Sentence>(splitChain(text, ['.', '?', '!']), (str: string) => {
+            return new Sentence(str);
+        });
     }
 
+}
+
+function splitChain(text: string, chain: string[]): string[] {
+    return _.flatten(_.map(splitParse(text, chain.shift()), function (str) {
+        return chain.length ? splitChain(str, chain.slice(0)) : str;
+    }));
+}
+
+function splitParse(text: string, symbol: string): string[] {
+    var r = _.compact(text.split(symbol)),
+        last = r.pop();
+
+    r = _.map(r, function (str) {
+        return str + symbol;
+    }).concat(last);
+
+    r = _.map(r, function (str) {
+        str = str.trim();
+        return str;
+    });
+
+    return r;
 }
 
 export = Sentence;
